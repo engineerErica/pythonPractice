fc7a47fa04d1670ead911b13814f58a8d8407874
Erica Ponce <ericaponce2002@gmail.com>
2025-03-06 12:16:21 -0500

    some week 7 and 8 problems completed

---
+++ week7_leetcodes/task7.py
@@ -0,0 +1,70 @@
+# Week 7: (Lab Day) Data Structures in Python
+
+""" In Class Task
+For this session we'll build upon what we've done for the last few sessions and work on
+Leetcode-style questions on your own. We'll go through the solutions toghether at the end.
+
+"""
+
+"""
+PROBLEM 1: Move Zeros
+https://leetcode.com/problems/move-zeroes/description/ 
+"""
+
+"""
+PROBLEM 2: Valid Parentheses
+https://leetcode.com/problems/valid-parentheses/description/
+"""
+
+"""
+PROBLEM 3: Majority Element
+https://leetcode.com/problems/majority-element/description/ 
+
+"""
+
+""" Post Class Task
+PROBLEM 1: Valid Parentheses
+https://leetcode.com/problems/valid-parentheses/description/
+"""
+"""
+1. Create a list/stack and a dictionary to match the brackets
+2. Iterate through the string
+    3. Check if the characters are not a pair: return false
+    4. Else add the open bracket to the list
+5. Check if the length of the stack is 0
+    6. If yes return true, else return false
+
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        my_stack = []
+        my_dict = {"]": "[", ")": "(", "}": "{"}
+        for char in s:
+            if char in my_dict and len(my_stack) > 0:
+                last_value = my_stack.pop()
+                if my_dict[char] != last_value:
+                    return False
+            else:
+                my_stack.append(char)
+        if len(my_stack) == 0:
+            return True
+        else:
+            return False
+
+"""
+PROBLEM 2 (extra practice): Majority Element
+https://leetcode.com/problems/majority-element/description/ 
+
+
+"""
+
+"""
+PROBLEM 3 (extra practice): Top K Frequent Elements
+https://leetcode.com/problems/top-k-frequent-elements/description/ 
+
+"""
---
+++ week8_leetcodes/task8.py
@@ -0,0 +1,90 @@
+""" In Class Task
+PROBLEM 1: Intersection of Two Arrays (Will go over this together)
+https://leetcode.com/problems/intersection-of-two-arrays/
+
+Brute Force Solution
+1. initialize output list as well as a set to keep track of only unique overlaps
+2. Iterate over nums1
+    3. Iterate over nums2
+        4. Check if the chars match and if the char is not in the set already
+            5. If true, add the char to the set and list
+        5. Else continue
+6. Return the list
+"""
+#Problem 1 Brute force solution
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        intersection = []
+        my_set = set()
+        for char1 in nums1:
+            for char2 in nums2:
+                if char1 == char2 and char1 not in my_set:
+                    my_set.add(char1)
+                    intersection.append(char1)
+        return intersection
+
+"""
+Optimized Solution:
+1. Initialize nums1 into a set
+2. Initialize result set
+3. Iterate over nums2
+    4. If the val from nums2 is in the set, add it to the result
+5. return the restult as a list
+"""
+#Problem 1 Optimized Solution
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set_nums1 = set(nums1)
+        result = set()
+        for num in nums2:
+            if num in set_nums1:
+                result.add(num)
+        return list(result)
+
+"""
+
+PROBLEM 2: Two Sum
+https://leetcode.com/problems/two-sum/description/
+
+1. Initialize a dictionary and an index variable
+2. Iterate over the list of nums
+    3. Initialize a variable to store the value that combines with the curr num to reach the target
+    4. if the supplemental value is a key in the dictionary, return the current index and the value at the key
+    5. Else initialize the curr num as a key in the dict with the value as the index
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        my_dict = {}
+        i = 0
+        for num in nums:
+            supplemental_val = target - num
+            if supplemental_val in my_dict:
+                return [my_dict[supplemental_val], i]
+            else:
+                my_dict[num] = i
+            i += 1
+
+"""
+Post Class Task:
+
+PROBLEM: Squares of a Sorted Array
+
+https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
+Hint: Use the sorted() funciton to sort a list in Python.
+"""
